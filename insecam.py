2d807a2acdf881272911c37539965cb66eb33a4a
Schildkroete1 <jonas.10@gmx.de>
2017-12-09 15:17:40 +0100

    Update insecam.py
    Now it uses position of the user to zoom on his home

--- insecam.py
+++ insecam.py
@@ -6,6 +6,7 @@ import os, sys
 import ftplib
 
 #Variablen:
+land = ''
 api_key = ''
 ftp_server = ''
 ftp_username = ''
@@ -26,12 +27,12 @@ with open (dateiname, 'w') as out:
   except:
     os.system("clear")
   
-  out.write('   <!DOCTYPE html>\n   <html>\n   <head>\n     <meta http-equiv="content-type" content="text/html; charset=ANSI" />\n     <title>Insecam Map</title>\n     <script src="https://maps.googleapis.com/maps/api/js?key=' + api_key + '&callback=initMap"\n      type="text/javascript"></script>\n   </head>\n   <body>\n     <div id="map" style="width: 1500px; height: 1000px;"></div>\n\n     <script type="text/javascript">\n       var locations = [')
+  out.write('   <!DOCTYPE html>\n   <html>\n   <head>\n     <meta http-equiv="content-type" content="text/html; charset=ANSI" />\n     <title>Insecam Map</title>\n     <script src="https://maps.googleapis.com/maps/api/js?key=' + api_key + '"\n      type="text/javascript"></script>\n   </head>\n   <body>\n     <div id="map" style="width: 1340px; height: 640px;"></div>\n\n     <script type="text/javascript">\n       var locations = [')
   url = []
-  r = requests.get('http://www.insecam.org/en/bycountry/DE/?page=1', headers={'user-agent': 'mozilla'}) #Seitenannzahl bestimmen
+  r = requests.get('http://www.insecam.org/en/bycountry/' + str(land) +'/?page=1', headers={'user-agent': 'mozilla'}) #Seitenannzahl bestimmen
   seiten = r.text.split('pagenavigator("?page=", ')[1].split(",")[0]
   for i in tqdm(range(1, int(seiten) + 1)): #Webcamannzahl und Webcamurl bestimmem
-    r = requests.get('http://www.insecam.org/en/bycountry/DE/?page=' + str(i), headers={'user-agent': 'mozilla'})
+    r = requests.get('http://www.insecam.org/en/bycountry/' + str(land) +'/?page=' + str(i), headers={'user-agent': 'mozilla'})
     for x in range (0, r.text.count('/en/view/')):
       url.append(str(r.text.split('/en/view/')[x+1].split('/"')[0]))
 
@@ -45,7 +46,7 @@ with open (dateiname, 'w') as out:
       out.write('        ["<a href=\'' + str(stream) + '\'><img id=\'image0\' src=\'' + str(stream) + '\' alt=\'\' height=\'420\' width=\'420\'/><b>' + str(city) + '</b></a>", ' + str(lat) + ', ' + str(lon) + ', "' + str(stream) + '"]];\n')
     if (i != len(url) - 1):
       out.write('        ["<a href=\'' + str(stream) + '\'><img id=\'image0\' src=\'' + str(stream) + '\' alt=\'\' height=\'420\' width=\'420\'/><b>' + str(city) + '</b></a>", ' + str(lat) + ', ' + str(lon) + ', "' + str(stream) + '"],\n')
-  out.write('var map = new google.maps.Map(document.getElementById("map"), {\n  zoom: 5,\n center: new google.maps.LatLng(51.54376, 9.910419999999931),\n mapTypeId: google.maps.MapTypeId.ROADMAP\n});\n\nvar infowindow = new google.maps.InfoWindow();\n\nvar marker, i;\n\nfor (i = 0; i < locations.length; i++) {\n  marker = new google.maps.Marker({\n   position: new google.maps.LatLng(locations[i][1], locations[i][2]),\n   map: map,\n   url: locations[i][3]\n  });\n\n google.maps.event.addListener(marker, "mouseover", (function(marker, i) {\n   return function() {\n     infowindow.setContent(locations[i][0]);\n     infowindow.open(map, marker);\n   }\n })(marker, i));\n\n      google.maps.event.addListener(marker, "click", (function(marker, i) {\n   return function() {\n     infowindow.setContent(locations[i][0]);\n     infowindow.open(map, marker);\n     window.location.href = this.url;\n   }\n })(marker, i));\n\n    }\n\n     </script>\n	 <!-- Google Analytics -->\n	<script>\n	(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){\n	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n	})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");\n\n	ga("create", "UA-110852907-1", "auto");\n	ga("send", "pageview");\n	</script>\n	<!-- End Google Analytics -->\n   </body>\n   </html>')
+  out.write('var map = new google.maps.Map(document.getElementById("map"), {\n  zoom: 5,\n center: new google.maps.LatLng(51.54376, 9.910419999999931),\n mapTypeId: google.maps.MapTypeId.ROADMAP\n});\n        if (navigator.geolocation) {\n          navigator.geolocation.getCurrentPosition(function(position) {\n            var pos = {\n              lat: position.coords.latitude,\n              lng: position.coords.longitude\n            };\n			map.setZoom(11);\n            map.setCenter(pos);\n          }, function() {\n            handleLocationError(true, infoWindow, map.getCenter());\n          });\n        } else {\n          handleLocationError(false, infoWindow, map.getCenter());\n        }\nvar infowindow = new google.maps.InfoWindow();\n\nvar marker, i;\n\nfor (i = 0; i < locations.length; i++) {\n  marker = new google.maps.Marker({\n   position: new google.maps.LatLng(locations[i][1], locations[i][2]),\n   map: map,\n   url: locations[i][3]\n  });\n\n      google.maps.event.addListener(marker, "click", (function(marker, i) {\n   return function() {\n     infowindow.setContent(locations[i][0]);\n     infowindow.open(map, marker);\n   }\n })(marker, i));\n\n    }\n\n     </script>\n	 <!-- Google Analytics -->\n	<script>\n	(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){\n	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n	})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");\n\n	ga("create", "UA-110852907-1", "auto");\n	ga("send", "pageview");\n	</script>\n	<!-- End Google Analytics -->\n   </body>\n   </html>\n')
   out.close()
 ftp = ftplib.FTP(ftp_server) #fertige html Datei über ftp hochladen
 ftp.login(ftp_username, ftp_password)
